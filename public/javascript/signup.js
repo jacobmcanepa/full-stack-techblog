fb42d54bf0d3aad255aa6307ec95312a97f2f4d3
Jacob Canepa <jacobmcanepa@gmail.com>
2022-10-06 16:22:33 -0700

    now sends user to homepage once signed up

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -18,6 +18,7 @@ async function signupFormHandler(event) {
 
     if (response.ok) {
       console.log('success');
+      document.location.replace('/');
     } else {
       alert(response.statusText);
     }
